b1d0a48eba20c38d5fb20aa66c122a31bfd046b4
AhmedAbdohOrtiga <eng.ahmed.ortiga.cena@gmail.com>
2014-06-23 21:28:52 +0200

    Update Bolinomial.cpp

--- Bolinomial.cpp
+++ Bolinomial.cpp
@@ -8,9 +8,13 @@
 #include "Bolinomial.h"
 #include <fstream>
 #include "SVMNode.h"
+#include "Node.h"
+#include <climits>
 
+Node* getNodeFromDisk(int id);
+int levelOfNode(int id);
 Bolinomial::Bolinomial() {
-	fstream tree("Polynomial\data.txt", std::ios_base::in);
+	fstream tree("Polynomial/data.txt", std::ios_base::in);
 	int id;
 	while (tree >> id) {
 		//(level,id)
@@ -20,36 +24,37 @@ Bolinomial::Bolinomial() {
 }
 
 Bolinomial::~Bolinomial() {
-	delete level_id;
+	delete &level_id;
 }
 void Bolinomial::consolidate(Node* node) {
 	while (level_id.find(node->get_level()) != level_id.end()) {
 		//merge 2 nodes
 		node = new SVM_Node(
-				getNodeFromDisk(level_id.find(node->get_level())->second),
-				node);
+				getNodeFromDisk(level_id.find(node->get_level())->second), node,
+				rand() % ULLONG_MAX);
 		//remove node
 		level_id.erase(node->get_level());
 	}
-	//level_id[node->get_level()]=node.getId();
+	level_id[node->get_level()] = node->get_num();
 
 }
 
-int Bolinomial::perdict(Mat* test) {
+int Bolinomial::perdict(Mat test) {
 	int retId = 0;
-	float max = 0;
+	float min = ULLONG_MAX;
 	for (map<int, int>::iterator it = level_id.begin(); it != level_id.end();
 			++it) {
 		Node* node = getNodeFromDisk(level_id[it->second]);
+		float val;
 		while (node->get_level() != 1) { //not leaf
-			if (node->predict(test, Mat()) == 0) { //go left
-
-			} else { //go right
-
+			if (node->predict(test, val) == 0) { //go left
+				node = Node::getInstance(node->get_left_child());
+				node = Node::getInstance(node->get_right_child());
 			}
 		}
-		if (node->predict(test, Mat()) > max)
-			retId = node.getId();
+		int id = node->predict(test, val);
+		if (val < min)
+			retId = id;
 	}
 	return retId;
 }
@@ -72,6 +77,7 @@ int levelOfNode(int id) {
 Node* getNodeFromDisk(int id) {
 	//it's supposed to read whole node according to how we'll save it
 	//if level 1 return knn node, else return svm
-	return NULL;	//to skip warning :D
+	Node* returnNode = Node::getInstance(id);
+	return returnNode;	//to skip warning
 }
 
